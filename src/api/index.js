3738b1e7230ed63d90b9c7ce6a4c794aa6d2e19c
Vladimir Kudinov <vlkudinov@gmail.com>
2019-06-12 19:40:01 +0300

    add proxy url

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,18 @@
-import axios from "axios";
+import axios from 'axios';
 
 export async function fetchRates(currency = 'EUR', period) {
-  
+  const proxy = 'https://cors-anywhere.herokuapp.com/';
   const date = new Date();
-  const today = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
+  const today = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
   date.setDate(date.getDate() - period);
-  const formattedDate = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
-  
+  const formattedDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+
   try {
-    const { data } = await axios.get(`https://api.exchangeratesapi.io/history?start_at=${formattedDate}&end_at=${today}&base=${currency}`);
-    
+    const { data } = await axios.get(
+      `${proxy}https://api.exchangeratesapi.io/history?start_at=${formattedDate}&end_at=${today}&base=${currency}`
+    );
+
     return await data;
-   
   } catch (e) {
     throw e;
   }
